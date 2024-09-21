dd8ca4452fc8270bcce3ff005ef2a93024fcfdf5
Alok3333 <alokprajapati697@gmail.com>
2024-09-21 22:05:41 +0530

    created ful name to display on submit clicked

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [fName, setFName] = useState("");
@@ -7,11 +7,50 @@ function App() {
   const [fullName, setFullName] = useState("");
   const [isError, setIsError] = useState(null);
 
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!fName || !lName) {
+      setIsError("Both first and last names must be field.");
+    } else {
+      setFullName(`${fName} ${lName}`);
+      setIsError(null);
+    }
+  };
+
   return (
-    <div style={{textAlign: "left", paddingLeft: "0.70%"}}>
-     <h1>Full Name Display</h1>
-     <form>
-     </form>
+    <div style={{ textAlign: "left", paddingLeft: "0.70%" }}>
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleFormSubmit}>
+        <label>First Name:</label>
+        <input
+          type="text"
+          name="fname"
+          value={fName}
+          onChange={(e) => setFName(e.target.value)}
+          required
+        />
+        <br />
+        <label>Last Name:</label>
+        <input
+          type="text"
+          name="lname"
+          value={lName}
+          onChange={(e) => setLName(e.target.value)}
+          required
+        />
+        <br />
+        {isError && (
+          <div>
+            <span style={{ color: "#FF0000" }}>{isError}</span>
+          </div>
+        )}
+        <div style={{ textAlign: "left", paddingLeft: "0.20%" }}>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+
+      {fullName && <p>Full Name: {fullName}</p>}
     </div>
   );
 }
